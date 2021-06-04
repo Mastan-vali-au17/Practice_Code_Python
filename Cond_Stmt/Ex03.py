74a7bf2f9097051e207359d4929e07969c462288
= <=>
2021-06-04 19:41:30 +0530

    Third commit

---
+++ Cond_Stmt/Ex03.py
@@ -0,0 +1,8 @@
+a = int(input("Enter A value:"))
+b = int(input("Enter B value:"))
+c = a+b
+print("The addition of c is:",c)
+if c%2==0:
+    print("The result of c is",c,"Even number")
+else:
+    print("the result of c is",c,"Odd number")    
